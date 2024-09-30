d7414fcc538315ec7b30cc5b12c1e6def3445f98
sudho712 <sudho712@gmail.com>
2024-09-30 11:34:24 +0530

    Food Management Website MERN Project

---
+++ src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+
+export default function Home() {
+  return (
+    <div>
+        <div><Navbar/></div>
+        <div>Body</div>
+        <div><Footer/></div>
+    </div>
+  );
+}
